9a7b454a5bf0659053c701e0944108fd3e66a2f5
Michael Mushrush <mushrushm@gmail.com>
2024-01-18 14:42:43 -0500

    this should work just fine

--- src/Component.jsx
+++ src/Component.jsx
@@ -7,6 +7,19 @@ const Component = () => {
       <div>Component has been modified by a new branch</div>
       <div>Component has been modified by a new branch</div>
       <div>Component has been modified by a new branch</div>
+      <div>Component has been modified by a new branch</div>
+      <div>Component has been modified by a new branch</div>
+      <div>Component has been modified by a new branch</div>
+      <div>Component has been modified by a new branch</div>
+      <div>Component has been modified by a new branch</div>
+      <div>Component has been modified by a new branch</div>
+      <div>Component has been modified by a new branch</div>
+      <div>Component has been modified by a new branch</div>
+      <div>Component has been modified by a new branch</div>
+      <div>Component has been modified by a new branch</div>
+      <div>Component has been modified by a new branch</div>
+      <div>Component has been modified by a new branch</div>
+      <div>Component has been modified by a new branch</div>
     </>
   )
 }
