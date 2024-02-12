2138beb71d744402ccab2af847da8c64fecf0e38
Michael Mushrush <mushrushm@gmail.com>
2024-02-12 08:35:59 -0500

    testing v.5

--- src/Component.jsx
+++ src/Component.jsx
@@ -9,10 +9,6 @@ const Component = () => {
       <div>Component has been modified by a new branch</div>
       <div>Component has been modified by a new branch</div>
       <div>Component has been modified by a new branch</div>
-      <div>Component has been modified by a new branch</div>
-      <div>Component has been modified by a new branch</div>
-      <div>Component has been modified by a new branch</div>
-      <div>Component has been modified by a new branch</div>
     </>
   )
 }
